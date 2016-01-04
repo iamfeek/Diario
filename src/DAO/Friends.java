b1dc6c53e4615c8156655b806147863da6be0cd3
Jy <piggy013130@yahoo.com.sg>
2016-01-04 09:59:33 +0800

    Added post encryption system

---
+++ src/DAO/Friends.java
@@ -0,0 +1,93 @@
+package DAO;
+
+import database.Db;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Friends {
+    public static boolean addFriend(String username, String friends_username) {
+        Connection conn = Db.getConnection();
+        String sql = "INSERT INTO diario.`friends` (username, f_username) VALUES (?, ?);";
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = conn.prepareStatement(sql);
+            preparedStmt.setString(1, username);
+            preparedStmt.setString(2, friends_username);
+            int status = preparedStmt.executeUpdate();
+            conn.close();
+            if (status == 1)
+                return true;
+            else
+                return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static ArrayList<String> getFriendsList(String username) {
+        ArrayList<String> friends_list = new ArrayList<String>();
+
+        Connection conn = Db.getConnection();
+        String query = "SELECT * FROM friends where username = '" + username + "' or f_username = '" + username + "';";
+
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                if (!rs.getString("f_username").equals(username))
+                    friends_list.add(rs.getString("f_username"));
+                else
+                    friends_list.add(rs.getString("usernames"));
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return friends_list;
+    }
+
+    public static boolean removeFriend(String username, String friends_username)   {
+        boolean tryOther = false;
+
+        Connection conn = Db.getConnection();
+        String sql = "DELETE FROM `diario`.`friends` WHERE `username`=? and`f_username`=?;";
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = conn.prepareStatement(sql);
+            preparedStmt.setString(1, username);
+            preparedStmt.setString(2, friends_username);
+            int status = preparedStmt.executeUpdate();
+            conn.close();
+            if (status == 1)
+                return true;
+            else
+                return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            tryOther = true;
+        }
+
+        if (tryOther)   {
+            conn = Db.getConnection();
+            sql = "DELETE FROM `diario`.`friends` WHERE `username`=? and`f_username`=?;";
+            preparedStmt = null;
+            try {
+                preparedStmt = conn.prepareStatement(sql);
+                preparedStmt.setString(1, friends_username);
+                preparedStmt.setString(2, username);
+                int status = preparedStmt.executeUpdate();
+                conn.close();
+                if (status == 1)
+                    return true;
+                else
+                    return false;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
